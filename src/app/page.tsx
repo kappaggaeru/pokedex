3c2cf1d83d97cfac6e5600482d7956f6d757f1b9
Lautaro Olivera <lautaro_olivera@EDSA-AR.com>
2025-01-01 23:45:02 -0300

    agrego h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h2>Pokedex is working!!</h2>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
