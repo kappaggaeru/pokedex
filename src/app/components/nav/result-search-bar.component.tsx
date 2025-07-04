52325d8ed46aaa257df5b7c2b87ca1e0088261e6
Lautaro Olivera <oliveralautaro99@gmail.com>
2025-07-04 18:33:23 -0300

    agrego borrar input

--- src/app/components/nav/result-search-bar.component.tsx
+++ src/app/components/nav/result-search-bar.component.tsx
@@ -1,4 +1,3 @@
-import { usePokemon } from "@/app/context/pokemonContext";
 
 type Props = {
     id: number;
@@ -6,12 +5,15 @@ type Props = {
     clearSearch: (id: number) => void;
 }
 export const ResultSearchBarComponent: React.FC<Props> = ({ id, name, clearSearch }) => {
+    
     return (
         <div className="
             border border-gray-200/50 dark:border-gray-600/50 
             p-2 px-4 rounded-xl bg-white dark:bg-slate-800
-            cursor-pointer 
-            group-hover/result:text-dark dark:hover:text-white"
+            hover:bg-slate-100 dark:hover:bg-slate-700
+            cursor-pointer group/result
+            group-hover/search:scale-100
+            "
             onClick={() => clearSearch(id)}
         >
             <p className="uppercase flex flex-row gap-2 items-center text-gray-500 dark:text-gray-400"><span className="text-sm text-gray-500">#{id}</span>{name}</p>
--- src/app/components/nav/search-bar.component.tsx
+++ src/app/components/nav/search-bar.component.tsx
@@ -1,4 +1,4 @@
-import { Search } from 'lucide-react';
+import { Search, X } from 'lucide-react';
 import { usePokemon } from '../../context/pokemonContext';
 import { useState } from 'react';
 import { ResultSearchBarComponent } from './result-search-bar.component';
@@ -42,46 +42,57 @@ export default function SearchBarComponent() {
     }
 
     return (
-        <div>
-            <div className={`
-                backdrop-blur-md rounded-full
-                border border-gray-200/50 dark:border-gray-600/50 shadow-lg
-                transition-transform duration-300 hover:scale-105 focus:scale-105
-                group/search
-                ${tier === "legendary"
-                    ? "bg-legendary"
-                    : tier === "mythical"
-                        ? "bg-mythical"
-                        : "bg-white/80 dark:bg-slate-800/80"
-                }
-            `}>
-                <div className={`px-6 py-1`}>
-                    <div className="flex items-center space-x-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-                        <Search />
-                        <input
-                            type="text"
-                            placeholder="Search"
-                            className="w-full bg-transparent p-2 focus:outline-none"
-                            name="searchPokemon"
-                            value={search}
-                            onChange={handleSearch}
-                        />
-                    </div>
+        <div className={`
+            backdrop-blur-md rounded-full
+            border border-gray-200/50 dark:border-gray-600/50 shadow-lg
+            transition-transform duration-300 
+            group/search hover:scale-105
+            ${tier === "legendary"
+                ? "bg-legendary"
+                : tier === "mythical"
+                    ? "bg-mythical"
+                    : "bg-white/80 dark:bg-slate-800/80"
+            }
+        `}>
+            <div className="px-6 py-1">
+                <div className="flex items-center space-x-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                    <Search />
+                    <input
+                        type="text"
+                        name="searchPokemon"
+                        value={search}
+                        placeholder="Search Pokémon"
+                        onChange={handleSearch}
+                        className="w-full bg-transparent p-2 focus:outline-none"
+                    />
+                    {search !== "" && (
+                        <button
+                            type="button"
+                            onClick={() => {
+                                setSearch("");
+                                setShowResults(false);
+                            }}
+                            className="absolute right-0 pr-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-600 dark:hover:text-gray-200"
+                        >
+                            <X className='w-6 h-6' />
+                        </button>
+                    )}
+                </div>
 
-                    {showResults && (
-                        <div className="
+                {showResults && (
+                    <div className="
                             backdrop-blur-md bg-white/80 dark:bg-slate-800/80
                             shadow-lg border border-gray-200/50 dark:border-gray-600/50 
                             absolute left-0 mt-4 z-40 p-4 pr-0
                             h-fit max-h-[15rem] w-full rounded-xl overflow-auto 
                         ">
-                            <div className='w-full h-full overflow-auto flex flex-col gap-2 pr-4 group/result'>
-                                {results.length > 0 ? results : <p className='text-gray-500 dark:text-gray-400'>No results</p>}
-                            </div>
+                        <div className='w-full h-full overflow-auto flex flex-col gap-2 pr-4'>
+                            {results.length > 0 ? results : <p className='text-gray-500 dark:text-gray-400'>No results</p>}
                         </div>
-                    )}
-                </div>
+                    </div>
+                )}
             </div>
         </div>
+
     );
 }
