e1ccb0fb3007e10dc23e6239871b4e9f407af72c
Lautaro Olivera <oliveralautaro99@gmail.com>
2025-07-12 19:06:20 -0300

    ajuste para aplicar theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className={`${geist.className} transition-colors duration-300`}>
-            <Head>
+            <head>
                 <meta name="theme-color" content="#eee"></meta>
+            </head>
+            <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link
