d859b16377ce46e7c724f2202f21d26a4d0f509b
Lautaro Olivera <oliveralautaro99@gmail.com>
2025-07-17 18:25:45 -0300

    borro codigo sin uso

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -2,15 +2,6 @@ const POKEMON_API_BASE_URL = 'https://pokeapi.co/api/v2';
 const ARTWORK_BASE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork';
 const SPRITE_BASE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
 
-// default call to get the total pokemon count and more
-export const getPokemonTotalCount = async () => {
-    const response = await fetch(`${POKEMON_API_BASE_URL}/pokemon`);
-    if (!response.ok) {
-        throw new Error('Error fetching pokemon count');
-    }
-    return response.json();
-}
-
 export const getPokedexData = async () => {
     const response = await fetch(`${POKEMON_API_BASE_URL}/pokedex/1/`);
     if (!response.ok) {
@@ -24,69 +15,6 @@ export const getPokemonById = async (id: number) => {
     const response = await fetch(`${POKEMON_API_BASE_URL}/pokemon/${id}`);
     if (!response.ok) {
         throw new Error('Error fetching pokemon');
-    }
-    return response.json();
-}
-
-// get all pokemon types
-export const getPokemonTypes = async () => {
-    const response = await fetch(`${POKEMON_API_BASE_URL}/type`);
-    if (!response.ok) {
-        throw new Error('Error fetching pokemon types');
-    }
-    return response.json();
-}
-
-// get pokemon type
-export const getPokemonTypeById = async (id: number) => {
-    const response = await fetch(`${POKEMON_API_BASE_URL}/type/${id}`);
-    if (!response.ok) {
-        throw new Error('Error fetching pokemon type');
-    }
-    return response.json();
-}
-
-// get all pokemon colors
-export const getPokemonColors = async () => {
-    const response = await fetch(`${POKEMON_API_BASE_URL}/pokemon-color`);
-    if (!response.ok) {
-        throw new Error('Error fetching pokemon colors');
-    }
-    return response.json();
-}
-
-// get pokemon color
-export const getPokemonColorById = async (id: number) => {
-    const response = await fetch(`${POKEMON_API_BASE_URL}/pokemon-color/${id}`);
-    if (!response.ok) {
-        throw new Error('Error fetching pokemon color');
-    }
-    return response.json();
-}
-
-// get all pokemon species
-export const getPokemonSpecies = async () => {
-    const response = await fetch(`${POKEMON_API_BASE_URL}/pokemon-species`);
-    if (!response.ok) {
-        throw new Error('Error fetching pokemon species');
-    }
-    return response.json();
-}
-
-// get pokemon species
-export const getPokemonSpeciesById = async (id: number) => {
-    const response = await fetch(`${POKEMON_API_BASE_URL}/pokemon-species/${id}`);
-    if (!response.ok) {
-        throw new Error('Error fetching pokemon species');
-    }
-    return response.json();
-}
-
-// get pokemon form
-export const getPokemonFormById = async (id: number) => {
-    const response = await fetch(`${POKEMON_API_BASE_URL}/pokemon-form/${id}`);
-    if (!response.ok) {
-        throw new Error('Error fetching pokemon form');
     }
     return response.json();
 }
