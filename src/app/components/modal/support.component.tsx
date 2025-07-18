dcbc04c5eeb8a9c896b6ead23d06b85d910025a2
Lautaro Olivera <oliveralautaro99@gmail.com>
2025-07-18 13:23:17 -0300

    integracion cafecito

--- src/app/components/modal/support.component.tsx
+++ src/app/components/modal/support.component.tsx
@@ -4,39 +4,30 @@ export const SupportComponent: React.FC = () => {
     const { setSpecialAchievement } = useAchievements();
 
     const handleClick = (platform: string) => {
-        if (platform == "cafecito") {
-            // link to cafecito
-        } else {
-            // link to other
-        }
+        console.log(platform);
         setSpecialAchievement(14);
     }
 
     return (
         <div className="flex flex-col">
-            <div className="text-gray-500 dark:text-gray-400 flex flex-col gap-4">
+            <div className="text-gray-500 dark:text-gray-400 flex flex-col gap-4 cursor-default">
                 <p className="font-bold">Support this developer — buy him a coffee!</p>
                 <p>This website was built by a single developer. Your support helps fund new features and, of course, coffee!</p>
                 <p>If you&#39;re in Argentina, you can use <i>Cafecito</i> to pay in ARS.</p>
-                <p>If you&#39;re in another country, use <i>PayPal</i> to pay in USD.</p>
                 <p>Thank you for your support!</p>
             </div>
             <div className="mt-4 flex flex-col gap-4">
-                <div>
-                    <div onClick={() => handleClick("cafecito")} className="
-                        bg-white dark:bg-slate-800 hover:bg-slate-100 dark:hover:bg-slate-700
-                        border border-gray-200/50 dark:border-gray-600/50 p-4 rounded-lg cursor-pointer"
-                    >
-                        <h5 className="text-gray-500 dark:text-gray-400 first-letter:uppercase">Cafecito</h5>
-                    </div>
-                </div>
-                <div>
-                    <div onClick={() => handleClick("paypal")} className="
-                        bg-white dark:bg-slate-800 hover:bg-slate-100 dark:hover:bg-slate-700
-                        border border-gray-200/50 dark:border-gray-600/50 p-4 rounded-lg cursor-pointer"
-                    >
-                        <h5 className="text-gray-500 dark:text-gray-400 first-letter:uppercase">PayPal</h5>
-                    </div>
+                <div onClick={() => handleClick("")} className="cursor-pointer w-fit">
+                    <a href='https://cafecito.app/kappaggaeru' rel='noopener' target='_blank'>
+                        <img
+                            srcSet='
+                                https://cdn.cafecito.app/imgs/buttons/button_5.png 1x,
+                                https://cdn.cafecito.app/imgs/buttons/button_5_2x.png 2x,
+                                https://cdn.cafecito.app/imgs/buttons/button_5_3.75x.png 3.75x'
+                            src='https://cdn.cafecito.app/imgs/buttons/button_5.png'
+                            alt='Invitame un café en cafecito.app'
+                        />
+                    </a>
                 </div>
             </div>
         </div>
